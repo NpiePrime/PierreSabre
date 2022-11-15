97fcbb379389d8736440b7caa46964c696af9517
zerkani yanis <zrn3963a@ens.fsi>
2022-11-15 17:44:24 +0100

    debut traitres

---
+++ src/personnages/Traitre.java
@@ -0,0 +1,13 @@
+package personnages;
+
+public class Traitre extends Samourai{
+
+	private int niveauTraitrise = 0;
+	public Traitre(String seigneur, String nom, String boissonFavorite, int argent) {
+		super(seigneur, nom, boissonFavorite, argent);
+
+	}
+	
+
+	
+}
